0096cf0b63666e3cc9d0fe92f59a0fc4790c7c0e
Olga <os@divan.tv>
2021-03-21 00:16:56 +0200

    add main structure of project

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,21 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <Header/>
+    <main role="main">
+      <router-view/>
+    </main>
+
   </div>
 </template>
+<script>
+  import Header from "@/components/Header";
+  export default{
+    name: "App",
+    components:{
+      Header
+    }
+  }
+</script>
 
 <style lang="scss">
 #app {
@@ -15,18 +24,12 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+  main{
+    background: #ccc;
+    height: calc(100vh - 70px);
   }
+
 }
+
+
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,114 @@
+<template>
+    <header>
+        <div class="logo-wrapper">
+            <router-link to="/" class="logo">
+                <img src="../assets/img/sociotrack-logo.svg" alt="logo">
+            </router-link>
+        </div>
+        <div class="right-header-content">
+            <router-link to="/" class="notification">
+                <span class="notification_indicator" v-if="notification">{{notification}}</span>
+            </router-link>
+            <button class="btn btn-search"></button>
+        </div>
+    </header>
+    
+</template>
+
+<script>
+    export default {
+        name: "Header",
+        data(){
+            return{
+                notification: 22
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    header{
+        display: flex;
+        box-sizing: border-box;
+        height: 70px;
+        padding-right: 15px;
+        .logo-wrapper{
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            width: 100%;
+            max-width: 240px;
+            background:  #2DBC80;
+            margin-right: auto;
+        }
+        .right-header-content{
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+        }
+        .notification{
+            position: relative;
+            display: block;
+            margin-right: 20px;
+            height: 30px;
+            width: 30px;
+            border-radius: 3px;
+            &:after{
+                position: absolute;
+                display: block;
+                content: '';
+                height: 30px;
+                width: 30px;
+                background: url("../assets/img/sub_nav/notification-icon.svg") no-repeat 50% 50%;
+                background-size: 60%;
+                transform: rotate(0);
+                will-change: transform;
+            }
+
+            &:hover{
+                /*background-color: rgba(#1CAF5E, .3);*/
+                &:after{
+                    animation: bell .3s linear 2;
+                }
+            }
+            &_indicator{
+                position: absolute;
+                right: -4px;
+                z-index: 1;
+                display: block;
+                height: 17px;
+                width: 17px;
+                border-radius: 50%;
+                color: #fff;
+                background: #1DCE6C;
+                font-size: 10px;
+                font-weight: 700;
+                line-height: 17px;
+                text-align: center;
+                vertical-align: middle;
+            }
+        }
+        .btn-search{
+            position: relative;
+            display: block;
+            margin-right: 20px;
+            height: 30px;
+            width: 30px;
+            background: url("../assets/img/sub_nav/search-icon.svg") no-repeat 50% 50%;
+            background-size: 60%;
+            border: none;
+            border-radius: 3px;
+            &:hover{
+                background-color: rgba(#1CAF5E, .3);
+            }
+        }
+    }
+    @keyframes bell{
+        from{transform: rotate(0);}
+        25%{ transform: rotate(-25deg);}
+        50%{transform: rotate(25deg);}
+        to{ transform: rotate(0);}
+
+    }
+
+</style>
